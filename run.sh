4ac486582f690d125583978fd651888206d7971d
wyx-2018 <43309265+wyx-2018@users.noreply.github.com>
2018-10-16 13:59:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-#python yad2k.py yolov2.cfg yolov2-voc.weights model_data/yolo.h5
+python yad2k.py yolov2.cfg yolov2.weights model_data/yolo.h5
 python test_yolo.py model_data/yolo.h5
-=======
-python yad2k.py yolov3.cfg yolov3.weights model_data/yolo.h5
->>>>>>> 556d07cd9c0b4412ce0f84b36e843f19c1013a88
